b578b0211ca897a701581896fd0ec3b67d8ed2aa
Zio7711 <zi.ao.tan@outlook.com>
2021-06-07 17:30:10 -0700

    Add DayListItem component and create a strorybook module

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,22 @@
-import React from "react";
-import "components/Button.scss";
-const classNames = require('classnames')
+import React from 'react';
+import 'components/Button.scss';
+const classNames = require('classnames');
 
 export default function Button(props) {
-   
-   let buttonClass = classNames('button', {
-      " button--confirm": props.confirm,
-      " button--danger": props.danger
-   })
+  let buttonClass = classNames('button', {
+    ' button--confirm': props.confirm,
+    ' button--danger': props.danger,
+  });
 
-   return (
-      <>
-         <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
-               {props.children}
-         </button>
-      </>
-      
-      );
+  return (
+    <>
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    </>
+  );
 }
