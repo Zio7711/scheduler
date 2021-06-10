254d2dbdd1dd51a2db2f8bb3b651f5bfebf0d99b
Zio7711 <zi.ao.tan@outlook.com>
2021-06-10 08:51:08 -0700

    Add helper function to get the appointment for specific date

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,57 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import DayList from './DayList';
 import Appointment from './Appointment';
+import { getAppointmentsForDay } from 'helpers/selectors';
 
 import 'components/Application.scss';
 import axios from 'axios'
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Zio Tan",
-      interviewer: { id: 2, name: 'Tori Malcolm', avatar: 'https://i.imgur.com/Nmx0Qxo.png' }
-    }
-  },
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "Zio Tan",
+//       interviewer: { id: 2, name: 'Tori Malcolm', avatar: 'https://i.imgur.com/Nmx0Qxo.png' }
+//     }
+//   },
 
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Matt Luo",
-      interviewer: { id: 3, name: 'Mildred Nazir', avatar: 'https://i.imgur.com/T2WwVfS.png' }
-    }
-  },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Matt Luo",
+//       interviewer: { id: 3, name: 'Mildred Nazir', avatar: 'https://i.imgur.com/T2WwVfS.png' }
+//     }
+//   },
 
-  {
-    id: 5,
-    time: "4pm",
-  },
-];
+//   {
+//     id: 5,
+//     time: "4pm",
+//   },
+// ];
 
 
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState('Monday');
-  const Appointments = appointments.map((appointment) => {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const setDay = day => setState({ ...state, day });
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+  
+  const Appointments = dailyAppointments.map((appointment) => {
     return (
       <Appointment 
         key={appointment.id}
@@ -61,13 +71,26 @@ export default function Application(props) {
   });
 
   useEffect( () => {
-    axios.get('http://localhost:8001/api/days')
-    .then( (res) => {
-      console.log("res --->", res.data);
-      setDays([...res.data])
-    
-    })
+
+    const getDaysURL = 'http://localhost:8001/api/days';
+    const getAppointmentsURL = 'http://localhost:8001/api/appointments';
+    const getInterviewersURL = 'http://localhost:8001/api/interviewers';
+
+    Promise.all([
+      axios.get(getDaysURL),
+      axios.get(getAppointmentsURL),
+      axios.get(getInterviewersURL),
+    ]).then((all) => {
+
+      const [days, appointments, interviewers] = all;
+      // console.log("Appointment DATA", appointments.data);
+      setState(prev => ({...prev, days: days.data, appointments: appointments.data, interviewers:interviewers.data}))
+
+    });
+
+
   }, []);
+
 
 
 
@@ -83,7 +106,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
